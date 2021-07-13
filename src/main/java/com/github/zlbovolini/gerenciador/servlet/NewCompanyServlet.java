3a80d0fc114ebcb2500838dacf40703b090e552f
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-07-13 17:07:39 -0300

    Refatoracao separado codigo HTML do codigo Java

--- src/main/java/com/github/zlbovolini/gerenciador/servlet/NewCompanyServlet.java
+++ src/main/java/com/github/zlbovolini/gerenciador/servlet/NewCompanyServlet.java
@@ -1,36 +1,35 @@
 package com.github.zlbovolini.gerenciador.servlet;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Optional;
 
 @WebServlet(urlPatterns = "/novaEmpresa")
 public class NewCompanyServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
         Optional<String> optionalName = Optional.ofNullable(req.getParameter("name"));
 
-        final PrintWriter writer = resp.getWriter();
+        if (optionalName.isPresent() && !optionalName.get().isBlank()) {
+            String name = optionalName.get();
 
-        optionalName.filter(name -> !name.isBlank())
-                .ifPresentOrElse(name -> {
+            Company company = new Company(name);
+            Database database = new Database();
 
-                    Company company = new Company(name);
-                    Database database = new Database();
+            database.save(company);
 
-                    database.save(company);
+            req.setAttribute("name", company.getName());
 
-                    writer.println("<html><body>Empresa " + name + " cadastrada com sucesso!</body></html>");
-                }, () -> {
-                    writer.println("<html><body>Por favor, informe o nome da empresa.</body></html>");
-                });
-
+            RequestDispatcher dispatcher = req.getRequestDispatcher("novaEmpresaCriada.jsp");
+            dispatcher.forward(req, resp);
+        }
     }
 }
--- src/main/java/com/github/zlbovolini/gerenciador/servlet/ShowCompanyServlet.java
+++ src/main/java/com/github/zlbovolini/gerenciador/servlet/ShowCompanyServlet.java
@@ -1,32 +1,26 @@
 package com.github.zlbovolini.gerenciador.servlet;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/listaEmpresas")
 public class ShowCompanyServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         Database database = new Database();
         List<Company> companies = database.findAll();
 
-        PrintWriter writer = resp.getWriter();
+        req.setAttribute("companies", companies);
 
-        writer.println("<html><body>");
-        writer.println("<ul>");
-
-        for (Company company : companies) {
-            writer.println("<li>" + company.getName() + "</li>");
-        }
-
-        writer.println("</ul>");
-        writer.println("</body></html>");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("listaEmpresas.jsp");
+        dispatcher.forward(req, resp);
     }
 }
